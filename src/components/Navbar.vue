26bb06245fb3e7b74324928016e453c61221811c
Mozeeb Abdulha <mozeebabdulha@gmail.com>
2020-02-01 23:10:05 +0000

    Added navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item @click.stop="drawer = !drawer" v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-action>
+            <v-icon class="black--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="black--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="cyan"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title dark>
+        <span class="font-weight-light">PickupN</span>
+        <span class="font-weight-bold">Dropoff</span>
+      </v-toolbar-title>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    links: [
+      { icon: 'mdi-home', text: 'Home', route: '/' }
+    ]
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/authentication/Register.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1 class="home">
+        Register Component
+    </h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
