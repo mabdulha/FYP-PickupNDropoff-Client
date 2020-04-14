837b46aea4b2b7b6428d108d34e132e85ca370b1
Mozeeb Abdulha <mozeebabdulha@gmail.com>
2020-04-14 11:55:37 +0100

    Making firebase.js public and using env variables

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from '@firebase/app'
+import 'firebase/storage'
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSANGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+}
+
+const fb = firebase.initializeApp(firebaseConfig)
+
+export { fb }
