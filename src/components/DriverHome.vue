ef2f56af72703503f3049b1251645f1e53a1f146
Mozeeb Abdulha <mozeebabdulha@gmail.com>
2020-04-19 14:15:12 +0100

    Accepting jobs working

--- src/components/DriverHome.vue
+++ src/components/DriverHome.vue
@@ -1,12 +1,18 @@
 <template>
-  <v-container fluid>
+  <div id="app1">
     <v-client-table :data="items" :columns="columns" :options="options">
+      <a slot="accept" slot-scope="props" class="fa fa-check-circle fa-2x" @click="submit(props.row._id)"></a>
+      <div slot="child_row" slot-scope="props">
+        <div> Pickup Address: {{ props.row.pLine1 + ', ' + props.row.pLine2 + ', ' + props.row.pTown + ', ' + props.row.pCounty + ', ' + props.row.pEircode }} </div>
+        <div> Dropoff Address: {{ props.row.dLine1 + ', ' + props.row.dLine2 + ', ' + props.row.dTown + ', ' + props.row.dCounty + ', ' + props.row.dEircode }} </div>
+      </div>
     </v-client-table>
-  </v-container>
+  </div>
 </template>
 
 <script>
 import DriverService from '../services/driverservice'
+import ItemService from '../services/itemservice'
 import Vue from 'vue'
 import VueTables from 'vue-tables-2'
 
@@ -17,16 +23,21 @@ export default {
       driver: {},
       driverID: this.$store.state.driver._id,
       items: [],
+      item: {},
+      props: ['_id'],
       options: {
         perPage: 10,
         headings: {
-          title: 'Title',
+          title: 'Item',
           size: 'Size',
-          pTown: 'pAddress',
-          datetime: 'Date and Time'
-        }
+          pEircode: 'Pickup Address',
+          dEircode: 'Dropoff Address',
+          datetime: 'Date and Time',
+          accept: 'Accept'
+        },
+        uniqueKey: '_id'
       },
-      columns: ['title', 'size', 'pAddress', 'aAddress', 'datetime', 'charge']
+      columns: ['title', 'size', 'datetime', 'charge', 'accept']
     }
   },
   mounted () {
@@ -42,6 +53,7 @@ export default {
       DriverService.fetchDriver(id)
         .then(response => {
           this.driver = response.data
+          console.log(this.driver)
         })
         .catch(err => {
           console.log(err)
@@ -57,6 +69,22 @@ export default {
         .catch(err => {
           console.log(err)
         })
+    },
+    submit (id) {
+      var item = {
+        status: 'In Transit',
+        driverID: this.driverID
+      }
+      this.item = item
+      this.updateItem(id, item)
+      console.log(item)
+    },
+    updateItem: function (id, item) {
+      console.log(id)
+      ItemService.updateItem(id, item)
+        .then(response => {
+          console.log(response)
+        })
     }
   },
   computed: {
@@ -66,3 +94,28 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+#app1 {
+  width: 95%;
+  margin: 0 auto;
+}
+
+.VueTables__child-row-toggler {
+  width:16px;
+  height:16px;
+  line-height: 16px;
+  display: block;
+  margin: auto;
+  text-align: center;
+}
+
+.VueTables__child-row-toggler--closed::before {
+   content: "+";
+}
+
+.VueTables__child-row-toggler--open::before  {
+    content: "-";
+}
+
+</style>
